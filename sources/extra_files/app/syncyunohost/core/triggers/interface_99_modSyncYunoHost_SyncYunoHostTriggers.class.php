34322c84e3128c1408b98e82291e1114bd5e0dfb
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-03-29 15:10:51 +0330

    Update interface_99_modSyncYunoHost_SyncYunoHostTriggers.class.php

--- sources/extra_files/app/syncyunohost/core/triggers/interface_99_modSyncYunoHost_SyncYunoHostTriggers.class.php
+++ sources/extra_files/app/syncyunohost/core/triggers/interface_99_modSyncYunoHost_SyncYunoHostTriggers.class.php
@@ -28,45 +28,59 @@ class InterfaceSyncYunoHostTriggers extends DolibarrTriggers
 	    // Retrieve YunoHost configuration values
 	    $yunohostBaseDomain = $conf->global->YUNOHOST_BASE_DOMAIN;
 	    $yunohostMainGroup = $conf->global->YUNOHOST_MAIN_GROUP;
-	    $yunohostOldUsers = 1;
-
+	    $check_dont_sync_with_yunohost = $this->check_dont_sync_with_yunohost($object);
+	    if($check_dont_sync_with_yunohost){
+	    	return 0;
+	    }
+	    $get_synced_with_yunohost = $this->get_synced_with_yunohost($object);
 	    // Handle actions using a switch statement
 	    switch ($action) {
 	        case 'MEMBER_CREATE':
 	            $fullName = $this->getFullName($object);
-		    $this->memberToUser($object->id);
-	            $this->runCommand('create', $object->login, $object->pass, $fullName, $object->email, $yunohostBaseDomain);
+	            $this->memberToUser($object->id);
+	            $create_output = $this->runCommand('create', $object->login, $object->pass, $fullName, $object->email, $yunohostBaseDomain);
+	            if ($this->check_user_created_or_exist($create_output, $object->login)) {
+                	$this->updateMemberExtraField($object->id, 'synced_with_yunohost', 1);
+	            }
 	            break;
 
 	        case 'MEMBER_SUBSCRIPTION_CREATE':
-	            $this->handleSubscriptionCreate($object, $yunohostBaseDomain, $yunohostMainGroup, $yunohostOldUsers);
+	            $this->handleSubscriptionCreate($object, $yunohostBaseDomain, $yunohostMainGroup, $get_synced_with_yunohost);
 	            break;
 
 	        case 'MEMBER_SUBSCRIPTION_DELETE':
 	        case 'MEMBER_SUBSCRIPTION_EXPIRED': // custum trigger by Syncyunohost
-	            $this->handleSubscriptionDelete($object, $yunohostBaseDomain, $yunohostMainGroup, $yunohostOldUsers);
+	            $this->handleSubscriptionDelete($object, $yunohostBaseDomain, $yunohostMainGroup, $get_synced_with_yunohost);
 	            break;
 
 	        case 'MEMBER_VALIDATE':
 	        case 'MEMBER_RESILIATE':
 	        case 'MEMBER_NEW_PASSWORD':
-	            if ($yunohostOldUsers) {
+	            if (!$get_synced_with_yunohost) {
 	                $fullName = $this->getFullName($object);
 	                $newPass = $this->generateSecurePassword(20);
-			$this->memberToUser($object->id);
-	                $this->runCommand('create', $object->login, $newPass, $fullName, $object->email, $yunohostBaseDomain);
+	                $create_output = $this->runCommand('create', $object->login, $newPass, $fullName, $object->email, $yunohostBaseDomain);
+	        		if ($this->check_user_created_or_exist($create_output, $object->login)) {
+	                	$this->updateMemberExtraField($object->id, 'synced_with_yunohost', 1);
+	                	$get_synced_with_yunohost = 1;
+	                }
 	            }
-	            if ($action === 'MEMBER_NEW_PASSWORD') {
-	                $this->runCommand('password', $object->login, $object->pass);
-	            }
+		        if ($get_synced_with_yunohost) {
+		            if ($action === 'MEMBER_NEW_PASSWORD') {
+		                $this->runCommand('password', $object->login, $object->pass);
+		            }
+		        }
 	            break;
 
 	        case 'MEMBER_MODIFY':
-	            $this->handleMemberModify($object, $yunohostOldUsers, $yunohostBaseDomain);
+	        	$get_synced_with_yunohost = $this->get_synced_with_yunohost($object);
+	            $this->handleMemberModify($object, $get_synced_with_yunohost, $yunohostBaseDomain);
 	            break;
 
 	        case 'MEMBER_DELETE':
-	            $this->runCommand('delete', $object->login);
+	            if ($get_synced_with_yunohost) {
+	            	$this->runCommand('delete', $object->login);
+	            }
 	            break;
 
 	        default:
@@ -77,7 +91,12 @@ class InterfaceSyncYunoHostTriggers extends DolibarrTriggers
 
 	    return 0;
 	}
-
+	private function check_dont_sync_with_yunohost($object) {
+	    return isset($object->array_options) ? ($object->array_options['options_dont_sync_with_yunohost'] ?? 0) : 0;
+	}
+	private function get_synced_with_yunohost($object) {
+	    return isset($object->array_options) ? ($object->array_options['options_synced_with_yunohost'] ?? 0) : 0;
+	}
 	private function getFullName($object)
 	{
 	    // Generate full name based on company or personal name
@@ -86,57 +105,101 @@ class InterfaceSyncYunoHostTriggers extends DolibarrTriggers
 	        : sprintf("%s %s", $object->firstname, $object->lastname);
 	}
 
-	private function handleSubscriptionCreate($object, $baseDomain, $mainGroup, $oldUsers)
+	private function handleSubscriptionCreate($object, $baseDomain, $mainGroup, $synced_with_yunohost)
 	{
 	    $member = new Adherent($this->db);
 	    if ($member->fetch($object->fk_adherent) > 0) {
-	        if ($oldUsers) {
+	        if (!$synced_with_yunohost) {
 	            $fullName = $this->getFullName($member);
 	            $newPass = $this->generateSecurePassword(20);
-	            $this->runCommand('create', $member->login, $newPass, $fullName, $member->email, $baseDomain);
+	            $create_output = $this->runCommand('create', $member->login, $newPass, $fullName, $member->email, $baseDomain);
+		        if ($this->check_user_created_or_exist($create_output, $object->login)) {
+		           	$synced_with_yunohost = 1;
+		           	$this->updateMemberExtraField($object->fk_adherent, 'synced_with_yunohost', 1);
+		        }
 	        }
-	        $this->runCommand('activate', $member->login, $mainGroup);
+	        if($synced_with_yunohost){
+	        	$this->runCommand('activate', $member->login, $mainGroup);
+	        }
 	    }
 	}
 
-	private function handleSubscriptionDelete($object, $baseDomain, $mainGroup, $oldUsers)
+	private function handleSubscriptionDelete($object, $baseDomain, $mainGroup, $synced_with_yunohost)
 	{
 	    $member = new Adherent($this->db);
 	    if ($member->fetch($object->fk_adherent) > 0) {
-	        if ($oldUsers) {
+	        if (!$synced_with_yunohost) {
 	            $fullName = $this->getFullName($member);
 	            $newPass = $this->generateSecurePassword(20);
-	            $this->runCommand('create', $member->login, $newPass, $fullName, $member->email, $baseDomain);
+	            $create_output = $this->runCommand('create', $member->login, $newPass, $fullName, $member->email, $baseDomain);
+		        if ($this->check_user_created_or_exist($create_output, $object->login)) {
+		           	$synced_with_yunohost = 1;
+		           	$this->updateMemberExtraField($object->fk_adherent, 'synced_with_yunohost', 1);
+		        }
 	        }
-	        $this->runCommand('deactivate', $member->login, $baseDomain, $mainGroup);
+	        if($synced_with_yunohost){
+	        	$this->runCommand('deactivate', $member->login, $baseDomain, $mainGroup);
+	        }
 	    }
 	}
 
-	private function handleMemberModify($object, $oldUsers, $baseDomain)
+	private function handleMemberModify($object, $synced_with_yunohost, $baseDomain)
 	{
-	    if ($oldUsers) {
+	    if (!$synced_with_yunohost) {
 	        $oldFullName = $this->getFullName($object->oldcopy);
 	        $newPass = $this->generateSecurePassword(20);
-	        $this->runCommand('create', $object->login, $newPass, $oldFullName, $object->oldcopy->email, $baseDomain);
+	        $create_output = $this->runCommand('create', $object->login, $newPass, $oldFullName, $object->oldcopy->email, $baseDomain);
+	        if ($this->check_user_created_or_exist($create_output, $object->login)) {
+	           	$synced_with_yunohost = 1;
+	           	$this->updateMemberExtraField($object->id, 'synced_with_yunohost', 1);
+	        }
+	    }
+	    if($synced_with_yunohost){
+		    $fullName = $this->getFullName($object);
+
+		    // Update email if it has changed
+		    if ($object->oldcopy->email !== $object->email) {
+		        $this->runCommand('modify_email', $object->login, $object->email);
+		    }
+
+		    // Update full name if it has changed
+		    if ($fullName !== $this->getFullName($object->oldcopy)) {
+		        $this->runCommand('modify_fullname', $object->login, $fullName);
+		    }
+
+		    // Update password if provided
+		    if ($object->pass) {
+		        $this->runCommand('password', $object->login, $object->pass);
+		    }	    	
 	    }
 
-	    $fullName = $this->getFullName($object);
-
-	    // Update email if it has changed
-	    if ($object->oldcopy->email !== $object->email) {
-	        $this->runCommand('modify_email', $object->login, $object->email);
-	    }
-
-	    // Update full name if it has changed
-	    if ($fullName !== $this->getFullName($object->oldcopy)) {
-	        $this->runCommand('modify_fullname', $object->login, $fullName);
-	    }
-
-	    // Update password if provided
-	    if ($object->pass) {
-	        $this->runCommand('password', $object->login, $object->pass);
-	    }
 	}
+	private function check_user_created_or_exist($create_output, $username){
+		if (strpos($create_output, 'SUCCESS User created') !== false ||  strpos($create_output, 'Error: User '.trim($username).' does exist') !== false) {
+			return true;
+		} else{
+			return false;
+		}
+	}
+	private function generateSecurePassword($length = 12)
+	{
+	    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
+	    $password = '';
+	    $charsLength = strlen($chars) - 1;
+
+	    for ($i = 0; $i < $length; $i++) {
+	        $password .= $chars[random_int(0, $charsLength)];
+	    }
+
+	    return $password;
+	}
+    private function updateMemberExtraField($member_id, $field_key, $field_value)
+    {
+		$sql = "UPDATE ".MAIN_DB_PREFIX."adherent_extrafields 
+		        SET ".$field_key." = ".$this->db->escape($field_value)."
+		        WHERE fk_object = ".$this->db->escape($member_id);
+		$this->db->query($sql);
+    }
 	private function memberToUser($member_id){
 		$found = 0;
 		$sql = "SELECT COUNT(rowid) as nb FROM ".MAIN_DB_PREFIX."user WHERE fk_member = ".((int) $member_id);
@@ -157,19 +220,6 @@ class InterfaceSyncYunoHostTriggers extends DolibarrTriggers
 			}
 		}
 	}
-	private function generateSecurePassword($length = 12)
-	{
-	    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
-	    $password = '';
-	    $charsLength = strlen($chars) - 1;
-
-	    for ($i = 0; $i < $length; $i++) {
-	        $password .= $chars[random_int(0, $charsLength)];
-	    }
-
-	    return $password;
-	}
-
 	private function runCommand($action, $username, $param1 = null, $param2 = null, $param3 = null, $param4 = null)
 	{
 	    // Sanitize arguments to prevent injection
@@ -186,4 +236,3 @@ class InterfaceSyncYunoHostTriggers extends DolibarrTriggers
 	    return shell_exec($cmd);
 	}
 }
-
